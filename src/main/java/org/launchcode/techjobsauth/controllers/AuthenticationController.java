9cf03403309d63d39bec6f989155bb061079da7b
nicolepeak <colecpeak@gmail.com>
2024-06-10 20:30:24 -0500

    working on studio

---
+++ src/main/java/org/launchcode/techjobsauth/controllers/AuthenticationController.java
@@ -0,0 +1,37 @@
+package org.launchcode.techjobsauth.controllers;
+
+import jakarta.servlet.http.HttpSession;
+import org.launchcode.techjobsauth.models.User;
+import org.launchcode.techjobsauth.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import java.util.Optional;
+
+@Controller
+public class AuthenticationController {
+
+    @Autowired
+    UserRepository userRepository;
+
+    private static final String userSessionKey = "user";
+
+    public User getUserFromSession(HttpSession session) {
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        if (userId == null) {
+            return null;
+        }
+
+        Optional<User> user = userRepository.findById(userId);
+
+        if (user.isEmpty()) {
+            return null;
+        }
+
+        return user.get();
+    }
+
+    private static void setUserInSession(HttpSession session, User user) {
+        session.setAttribute(userSessionKey, user.getId());
+    }
+}
